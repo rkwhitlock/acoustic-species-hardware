4b647542a2c7b79958de9f84f5509f58bd4faf43
zclawr <zclawrence@ucsd.edu>
2025-05-31 16:55:35 -0700

    added code gen for inference without validation on target

---
+++ CM7/X-CUBE-AI/App/ai_io_buffers_ATON.h
@@ -0,0 +1,26 @@
+/**
+ ******************************************************************************
+ * @file    ai_io_buffers_ATON.h
+ * @author  MCD/AIS Team
+ * @brief   AI Validation application (entry points) - IO buffers allocation
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#ifndef AI_IO_BUFFERS_ATON_H
+#define AI_IO_BUFFERS_ATON_H
+
+/* ------ Utilities prototypes ------ */
+void connect_input_buffers(void);
+void connect_output_buffers(void);
+
+#endif  /* AI_IO_BUFFERS_ATON_H */
